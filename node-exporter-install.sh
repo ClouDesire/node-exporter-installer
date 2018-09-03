7eb613be76d55b4d8b42698c908c4d5d1f515931
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-09-03 17:58:50 +0200

    Support upstart

--- node-exporter-install.sh
+++ node-exporter-install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 VERSION=0.16.0
 
 if [ -x "$(command -v apt-get)" ]; then
@@ -7,7 +7,7 @@ if [ -x "$(command -v apt-get)" ]; then
 elif [ -x "$(command -v yum)" ]; then
     yum -y install wget
 else
-    echo "Err: no path to apt-get or yum" >&2;
+    echo "No known package manager found" >&2;
     exit 1;
 fi
 
@@ -18,7 +18,8 @@ mkdir -p /opt/node_exporter
 mv node_exporter-${VERSION}.linux-amd64/node_exporter /opt/node_exporter/
 rm -rf /tmp/node_exporter-${VERSION}.linux-amd64
 
-cat << EOF > /lib/systemd/system/node-exporter.service
+if [ -x "$(command -v systemctl)" ]; then
+    cat << EOF > /lib/systemd/system/node-exporter.service
 [Unit]
 Description=Prometheus agent
 After=network.target
@@ -34,5 +35,19 @@ ExecStart=/opt/node_exporter/node_exporter
 WantedBy=multi-user.target
 EOF
 
-systemctl enable node-exporter
-systemctl start node-exporter
+    systemctl enable node-exporter
+    systemctl start node-exporter
+elif [ -x "$(command -v initctl)" ]; then
+    cat << EOF > /etc/init/node-exporter.conf
+start on runlevel [23456]
+stop on runlevel [016]
+exec /opt/node_exporter/node_exporter
+respawn
+EOF
+
+    initctl reload-configuration
+    initctl start node-exporter
+else
+    echo "No known service management found" >&2;
+    exit 1;
+fi
