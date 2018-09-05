170ab6db3c575c0acf6ce390121d67a5d647d322
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-09-05 16:05:25 +0200

    Use cURL instead of wget (#7)

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 VERSION=0.16.0
+RELEASE=node_exporter-${VERSION}.linux-amd64
 
 _check_root () {
     if [ $(id -u) -ne 0 ]; then
@@ -8,16 +9,16 @@ _check_root () {
     fi
 }
 
-_install_wget () {
-    if [ -x "$(command -v wget)" ]; then
+_install_curl () {
+    if [ -x "$(command -v curl)" ]; then
         return
     fi
 
     if [ -x "$(command -v apt-get)" ]; then
         apt-get update
-        apt-get -y install wget
+        apt-get -y install curl
     elif [ -x "$(command -v yum)" ]; then
-        yum -y install wget
+        yum -y install curl
     else
         echo "No known package manager found" >&2;
         exit 1;
@@ -25,14 +26,13 @@ _install_wget () {
 }
 
 _check_root
-_install_wget
+_install_curl
 
 cd /tmp
-wget https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz
-tar xvfz node_exporter-${VERSION}.linux-amd64.tar.gz
+curl -sSL https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/${RELEASE}.tar.gz | tar xz
 mkdir -p /opt/node_exporter
-mv node_exporter-${VERSION}.linux-amd64/node_exporter /opt/node_exporter/
-rm -rf /tmp/node_exporter-${VERSION}.linux-amd64
+mv ${RELEASE}/node_exporter /opt/node_exporter/
+rm -rf /tmp/${RELEASE}
 
 if [ -x "$(command -v systemctl)" ]; then
     cat << EOF > /lib/systemd/system/node-exporter.service
